5773ef9ce1bf3dacca676a873aaf09de5d91752f
chinmay-abhyankar <chinmaynilesh93@gmail.com>
2025-07-22 18:15:30 +0530

    22-07 localstack setup

---
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -0,0 +1,24 @@
+package com.pm.stack;
+
+import software.amazon.awscdk.*;
+
+public class LocalStack extends Stack {
+
+    public LocalStack(final App scope, final String id, final StackProps props){
+        super(scope,id,props);
+    }
+
+    public static void main(String[] args) {
+        App app = new App(AppProps.builder().outdir("./cdk.out").build());
+
+        //java code to cloud formation template
+        StackProps stackProps = StackProps.builder().synthesizer(new BootstraplessSynthesizer())
+                .build();
+
+        new LocalStack(app,"localstack",stackProps);
+        app.synth();
+
+        System.out.println("App synthesizing in progress");
+    }
+
+}
